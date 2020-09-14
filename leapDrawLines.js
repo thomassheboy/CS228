011a73ba7512a180c9b7bf29efb51edef52949b3
thomassheboy <56599643+thomassheboy@users.noreply.github.com>
2020-09-14 14:23:13 -0400

    Completed

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -21,7 +21,12 @@ function HandleFrame(frame){
 function HandleHand(hand,pointables){
 	//console.log(hand);
 	var fingers = hand.fingers;
-	HandleFinger(fingers,pointables);
+	for (i = 3 ; i >= 0 ; i--){
+		for (j = 0 ; j < 5 ; j++){
+		HandleBone(fingers[j].bones[i],i);
+		}	
+	}	
+	//HandleFinger(fingers,pointables);
 
 }
 
@@ -29,8 +34,8 @@ function HandleFinger(fingers,pointables){
 	//console.log(fingers);
 	for (j = 0; j < 5; j++) {
 		
-			console.log(fingers[j]);
-			var tip = pointables[1].tipPosition;
+			//console.log(fingers[j]);
+			var tip = pointables[j].tipPosition;
 			x = tip[0];
 			y = tip[1];
 			z = tip[2];
@@ -50,13 +55,81 @@ function HandleFinger(fingers,pointables){
 
 			scalex = (((x + window.innerWidth) - rawXMin) / (rawXMax - rawXMin) ) * (window.innerWidth);
 			scaley = (((-y + window.innerHeight) - rawYMin) / (rawYMax - rawXMin) ) * (window.innerHeight);
-			circle(x + window.innerWidth/2,-y + window.innerHeight,r);
-			console.log(scalex);
-			console.log(scaley);
-
-		
+			//circle(x + window.innerWidth/2,-y + window.innerHeight,r);
+			//console.log(scalex);
+			//console.log(scaley);
+			var bones = fingers[j].bones;
+			HandleBone(bones);
 	}
 }
+
+function HandleBone(bones,i){
+	//for (i = 0; i < 4 ; i++){
+		//console.log(bones[i]);
+		var x = bones.nextJoint[0];
+		var y = bones.nextJoint[1];
+		var z = bones.nextJoint[2];
+		var a = bones.prevJoint[0];
+		var b = bones.prevJoint[1];
+		var c = bones.prevJoint[2];
+		
+		if (x < rawXMin){
+				rawXMin = x;
+			}
+			if (x > rawXMax){
+				rawXMax = x;
+			}
+			if (y < rawYMin){
+				rawYMin = y;
+			}
+			if (y > rawYMax){
+				rawYMax = y;
+			}
+			
+
+			scalex = (((x + window.innerWidth) - rawXMin) / (rawXMax - rawXMin) ) * (window.innerWidth);
+			scaley = (((-y + window.innerHeight) - rawYMin) / (rawYMax - rawXMin) ) * (window.innerHeight);
+			//circle(x + window.innerWidth/2,-y + window.innerHeight,r);
+			[a,b,x,y] = transformCoordinates(a,b,x,y);
+			if (i == 0){
+				stroke(100);
+				strokeWeight(4);
+			}else if (i == 1){
+				stroke(75);
+				strokeWeight(3);
+			}else if (i == 2){
+				stroke(50);
+				strokeWeight(2);
+			}else if (i == 3){
+				stroke(25);
+				strokeWeight(1);
+			}
+			line(a,b,x,y);
+	//}	
+}
+
+function transformCoordinates(a,b,x,y){
+	if (x < rawXMin){
+		rawXMin = x;
+	}
+	if (x > rawXMax){
+		rawXMax = x;
+	}
+	if (y < rawYMin){
+		rawYMin = y;
+	}
+	if (y > rawYMax){
+		rawYMax = y;
+	}
+			
+	scalex = (((x + window.innerWidth) - rawXMin) / (rawXMax - rawXMin) ) * (window.innerWidth);
+	scaley = (((-y + window.innerHeight) - rawYMin) / (rawYMax - rawXMin) ) * (window.innerHeight);
+	x = x + window.innerWidth/2;
+	a = a + window.innerWidth/2;
+	y = -y + window.innerHeight/2;
+	b = -b + window.innerHeight/2;
+	return [a,b,x,y];
+}	
 
 Leap.loop(controllerOptions, function(frame)
 {	
